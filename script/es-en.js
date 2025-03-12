5c598bf8c53074215bc2707a979bef7df62bd290
oreogit44 <mrequena@fie.undef.edu.ar>
2025-03-12 00:57:01 -0300

    final commit

--- script/es-en.js
+++ script/es-en.js
@@ -1,8 +1,6 @@
 const mainDescription = document.querySelector('.main--description');
 const aboutMe = document.querySelector('#aboutMe');
 const aMDescription1 = document.querySelector('.aboutMe--description1');
-const descriptionDegree = document.querySelector('.description--degree');
-const descriptionInterest = document.querySelector('.description--interest');
 const aMDescription2 = document.querySelector('.aboutMe--description2');
 const stackLanguages = document.querySelector('#languages');
 const stackTools = document.querySelector('#tools');
@@ -10,7 +8,7 @@ const stackLearning = document.querySelector('#learning');
 const contactMe = document.querySelector('#contactMe');
 const lanButton = document.getElementById('toggle-language');
 
-let isEnglish = true;  // A flag to track the current language
+let isEnglish = false; 
 
 function languageSwitch() {
     lanButton.addEventListener('mouseenter', function(){
@@ -19,27 +17,25 @@ function languageSwitch() {
 
     lanButton.addEventListener('click', function(){
         if (isEnglish) {
-            // Switch to Spanish
-            mainDescription.innerHTML = 'ESTUDIANTE DE INGENIERÍA DE SOFTWARE';
-            aboutMe.innerHTML = 'SOBRE MÍ';
-            aMDescription1.innerHTML = 'Estudiante de cuarto año de Ingeniería de Software, interesado en desarrollo Full Stack';
-            aMDescription2.innerHTML = 'Me esfuerzo por entregar proyectos y presentaciones de alta calidad, prestando atención al detalle y buscando continuamente la mejora. Soy una persona curiosa, rápida para aprender nuevas herramientas a través del aprendizaje continuo, y comprometida con resolver desafíos de manera creativa.';
-            stackLanguages.innerHTML = 'IDIOMAS';
-            stackTools.innerHTML = 'OTRAS HERRAMIENTAS';
-            stackLearning.innerHTML = 'APRENDIENDO ACTUALMENTE';
-            contactMe.innerHTML = 'CONTÁCTAME';
+            mainDescription.innerHTML = 'ESTUDIANTE DE INGENIERÍA INFORMÁTICA';
+            aboutMe.innerHTML = 'SOBRE MI';
+            aMDescription1.innerHTML = 'Estudiante de cuarto año de <strong>Ingeniería en Informática</strong>, con interés en el desarrollo <strong>Full Stack</strong>.';
+            aMDescription2.innerHTML = 'Me esfuerzo por ofrecer proyectos y presentaciones de alta calidad, prestando atención a los detalles y buscando siempre mejorar los resultados. Soy una persona curiosa, rápida para aprender nuevas herramientas mediante el aprendizaje continuo y comprometida con la resolución creativa de desafíos.';
+            stackLanguages.innerHTML = 'LENGUAJES';
+            stackTools.innerHTML = 'CONOCIMIENTOS Y HERRAMIENTAS';
+            stackLearning.innerHTML = 'ACTUALMENTE APRENDIENDO';
+            contactMe.innerHTML = 'CONTACTAME';
         } else {
-            // Switch to English
             mainDescription.innerHTML = 'SOFTWARE ENGINEERING STUDENT';
             aboutMe.innerHTML = 'ABOUT ME';
-            aMDescription1.innerHTML = 'Fourth year Software Engineering Student, interested in Full Stack development';
+            aMDescription1.innerHTML = 'Fourth year <strong>Software Engineering Student</strong>, interested in <strong>Full Stack development</strong>.';
             aMDescription2.innerHTML = 'I make an effort to deliver high-quality projects and presentations, paying close attention to detail and continuously seeking improvement. I am a curious person, quick to learn new tools through continuous learning, and committed to creatively solving challenges.';
             stackLanguages.innerHTML = 'LANGUAGES';
             stackTools.innerHTML = 'OTHER TOOLS';
             stackLearning.innerHTML = 'CURRENTLY LEARNING';
             contactMe.innerHTML = 'CONTACT ME';
         }
-        isEnglish = !isEnglish;  // Toggle the language
+        isEnglish = !isEnglish;
     });
 }
 
